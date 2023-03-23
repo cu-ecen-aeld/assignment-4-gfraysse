8e7c2014f2fb9419cc642de025a7bb46dd8e1e36
Guillaume Fraysse <gfraysse.ml@gmail.com>
2023-03-23 13:45:35 +0100

    assignment-4-complete

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ then
 	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
 	then
 		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		make -j4 -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} CCACHE_OPTIONS="--zero-stats" ccache-options CCACHE_OPTIONS="--max-size=5G"
+		make -j4 -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} 
 	else
 		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
